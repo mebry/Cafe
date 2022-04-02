7f234d2b62aa785369a6aeafc6b0d269187d7bfa
Mebry <vadushko@gmail.com>
2022-04-02 13:05:52 +0300

    Added a description of the classes. Added checks on the entered data.

--- Cafe.Data/Storage/Json/IngredientsStorageJson.cs
+++ Cafe.Data/Storage/Json/IngredientsStorageJson.cs
@@ -6,19 +6,37 @@ using Cafe.Bll;
 
 namespace Cafe.Data.Storage.Json
 {
+    /// <summary>
+    /// A class for writing and reading data from a file by serialization and deserialization.
+    /// </summary>
     public class IngredientsStorageJson : ISerializationData<IIngredientsStorage>
     {
-        [JsonIgnore]
         private List<IIngredientsStorage> _ingredientsStorage;
         private string _path;
 
+        /// <summary>
+        /// Constructor for filling in data.
+        /// </summary>
+        /// <param name="path">The path where the file will be contained.</param>
+        /// <param name="ingredientsStorage">List of elements.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         public IngredientsStorageJson(string path, List<IIngredientsStorage> ingredientsStorage)
         {
+            if(path==null)
+                throw new ArgumentNullException(nameof(path));
+
+            if (path == "")
+                throw new ArgumentException(nameof(path));
+
+            if(ingredientsStorage==null)
+                throw new ArgumentNullException(nameof(ingredientsStorage));
+
             _ingredientsStorage = ingredientsStorage;
             _path = path;
         }
 
-        public void Read()
+        public void Write()
         {
             SerializeObject.Serialize<IIngredientsStorage>(_path, _ingredientsStorage);
         }
--- Cafe.Data/Storage/Json/PlatterJsonData.cs
+++ Cafe.Data/Storage/Json/PlatterJsonData.cs
@@ -9,19 +9,19 @@ namespace Cafe.Data.Storage.Json
     /// <summary>
     /// A class for writing and reading data from a file by serialization and deserialization.
     /// </summary>
-    public class OrderJsonData : ISerializationData<IOrder>
+    public class PlatterJsonData : ISerializationData<IPlatter>
     {
-        private List<IOrder> _orders;
+        private List<IPlatter> _platters;
         private string _path;
 
         /// <summary>
         /// Constructor for filling in data.
         /// </summary>
         /// <param name="path">The path where the file will be contained.</param>
-        /// <param name="orders">List of elements.</param>
+        /// <param name="platters">List of elements.</param>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="ArgumentException"></exception>
-        public OrderJsonData(string path, List<IOrder> orders)
+        public PlatterJsonData(string path, List<IPlatter> platters)
         {
             if (path == null)
                 throw new ArgumentNullException(nameof(path));
@@ -29,23 +29,30 @@ namespace Cafe.Data.Storage.Json
             if (path == "")
                 throw new ArgumentException(nameof(path));
 
-            if (orders == null)
-                throw new ArgumentNullException(nameof(orders));
+            if (platters == null)
+                throw new ArgumentNullException(nameof(platters));
 
-            _orders = orders;
+            _platters = platters;
             _path = path;
         }
 
+        /// <summary>
+        /// The method responsible for writing the data.
+        /// </summary>
         public void Write()
         {
-            SerializeObject.Serialize<IOrder>(_path, _orders);
+            SerializeObject.Serialize<IPlatter>(_path, _platters);
         }
 
+        /// <summary>
+        /// The method responsible for reading the data.
+        /// </summary>
+        /// <param name="jsonConverter"></param>
         public void Restore(params JsonConverter[] jsonConverter)
         {
-            _orders=DeserializeObject.Deserialize<IOrder>(_path, jsonConverter).ToList();
+            _platters = DeserializeObject.Deserialize<IPlatter>(_path, jsonConverter).ToList();
         }
 
-        public IEnumerable<IOrder> GetAll() => _orders;
+        public IEnumerable<IPlatter> GetAll() => _platters;
     }
 }
