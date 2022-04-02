7107bb2b34f185959d63f1f0bf804560a7553e3a
Mebry <vadushko@gmail.com>
2022-04-02 13:04:44 +0300

    Updating interfaces. The main interface is divided into several subsections and inherited from them.

---
+++ Cafe.DI/SerializationInterfaces/IDeserialize.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Cafe.DI.SerializationInterfaces
+{
+    /// <summary>
+    /// An interface describing a contract that implements data deserialization.
+    /// </summary>
+    public interface IDeserialize
+    {
+        void Restore(params JsonConverter[] jsonConverter);
+    }
+}
