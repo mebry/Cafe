c454d4f2ab5009d4750875a7a94edaf134d01fc5
Mebry <vadushko@gmail.com>
2022-04-03 00:42:21 +0300

    Added a description.

--- Cafe.DI/Interfaces/Models/IMeal.cs
+++ Cafe.DI/Interfaces/Models/IMeal.cs
@@ -3,6 +3,9 @@ using Cafe.DI.Enums;
 
 namespace Cafe.DI.Interfaces.Models
 {
+    /// <summary>
+    /// Interface describing the user's order.
+    /// </summary>
     public interface IMeal:IData<(TypeOfProduct,string)>
     {
         void Reset();
