c7c5a79298a4ee6e9d626e5d121d2223f868cfb2
Mebry <vadushko@gmail.com>
2022-04-02 17:50:05 +0300

    Added a converter for the Processing class.

---
+++ Cafe.Data/Storage/File/Converters/ProcessingConverter.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Converters;
+using Cafe.DI.Interfaces.Operation.Processing;
+using Cafe.DI.Enums;
+using Cafe.Bll.Models;
+
+namespace Cafe.Data.Storage.Converters
+{
+    public class ProcessingConverter : CustomCreationConverter<IProcessing>
+    {
+        public override IProcessing Create(Type objectType)
+        {
+            return new Processing(1, new TimeSpan(0, 0, 1),ProcessingType.NoProcessing);
+        }
+    }
+}
